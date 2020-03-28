f621212585728e4c60ebf26d0328840b7e67f562
henriquearaujo93 <you@example.com>
2020-03-28 15:38:43 +0000

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+//Importar css global
+import './global.css';
+
+//Importar Rotas
+import Routes from './routes';
+
+//JSX - Javascript XML - Arquivo html escrito dentro do javascript
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!! By Henrique Araújo.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //Componentes
+    <Routes></Routes>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'; //Integração do react com o DOm
+import App from './App'; //Importar o app.js
 
+//Componente - Função que retorna html;
+//Renderizar o <App/> dentro da div com id="root" 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
